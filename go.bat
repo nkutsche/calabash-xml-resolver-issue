30ddc0c3fb180aadfeb4b7f069526c7cbf68919c
Nico Kutscherauer <kutscherauer@schematron-quickfix.com>
2023-05-23 14:44:02 +0200

    Cleans up the go.bat

--- go.bat
+++ go.bat
@@ -4,18 +4,9 @@ set CLBSH_VERSION=1.5.4-100
 set JAVA_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005
 set JAVA_OPTS=
 
-set MAVEN_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005
-set MAVEN_OPTS=
-
 echo %DATE% %TIME%
 
 
-::java %JAVA_OPTS% -cp "libs/xmlcalabash-%CLBSH_VERSION%/xmlcalabash-%CLBSH_VERSION%.jar;../../../core/target/epd.converter-1.0.0-SNAPSHOT-module100.jar" -Dcom.xmlcalabash.xproc-configurer=com.nkutsche.xslt.pkg.handler.XProcConfigurer com.xmlcalabash.drivers.Main main.xpl
-
-::java %JAVA_OPTS% -cp "libs/xmlcalabash-%CLBSH_VERSION%/xmlcalabash-%CLBSH_VERSION%.jar;../module" com.xmlcalabash.drivers.Main main.xpl
-
-set ARGS=%JAVA_OPTS% -cp %%classpath;module com.xmlcalabash.drivers.Main main.xpl
-
-mvn exec:exec -Dexec.executable="java" -Dexec.args="%ARGS%"
+mvn -f module/pom.xml install && mvn exec:exec -Dexec.executable="java"
 
 echo %DATE% %TIME%
